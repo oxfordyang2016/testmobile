1d9480d62838e8a8e157d910028fc3f2888e31e5
yangming <yang756260386@gmail.com>
2017-08-21 10:22:14 +0800

    allok

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask,redirect, url_for,request,make_response
+from flask import  render_template
+app = Flask(__name__)
+
+
+
+
+
+
+@app.route('/')
+def start():
+    #return 'hello world, newzealand!!!!!'
+    return render_template('index.html')
+
+
+
+@app.route('/treatments')
+def price():
+    return render_template('treatments.html')
+
+
+
+@app.route('/aboutus')
+def abus():
+    return render_template('aboutus.html')
+
+
+
+@app.route('/coverage')
+def coverage():
+    return render_template('coverage.html')    
+
+
+@app.route('/appointment')
+def appointment():
+    return render_template('appointment.html')
+
+
+@app.route('/google9014931f0be3f413.html')
+def goolecheck():
+    return render_template('google9014931f0be3f413.html')
+
+
+
+@app.route('/test')
+def test():
+    return 'designed by jack yang,please communicate with yang756260386@gmail.com 233 '
